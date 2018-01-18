783adafc6cf9b300591ee40a4a9e9c352b65e0ab
Kevin Mulkearn <kevinmulkearn@gmail.com>
2018-01-18 20:19:59 +0000

    HSV information added. Hex information added. Design updated.

--- app/src/main/java/com/mulkearn/kevin/colorpicker/MainActivity.java
+++ app/src/main/java/com/mulkearn/kevin/colorpicker/MainActivity.java
@@ -13,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
     RelativeLayout myLayout;
     SeekBar redSeeker, greenSeeker, blueSeeker;
-    TextView redValue, greenValue,  blueValue;
+    TextView redValue, greenValue,  blueValue, hexValue, hueValue, satValue, valValue;
     private int red_value, green_value, blue_value;
+    private String hex;
+    private float hue, sat, val;
+    private float[] hsv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         blueValue = (TextView) findViewById(R.id.blueValue);
         blueValue.setFilters(new InputFilter[]{ new InputFilterMinMax("0", "255")});
 
+        hexValue = (TextView) findViewById(R.id.hexValue);
+
+        hueValue = (TextView) findViewById(R.id.hueValue);
+        satValue = (TextView) findViewById(R.id.satValue);
+        valValue = (TextView) findViewById(R.id.valValue);
+
 
         redSeeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressChangedValue = 0;
@@ -46,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
                 progressChangedValue = progress;
                 redValue.setText(Integer.toString(progressChangedValue));
                 getBackColor();
+                getHexValue();
+                getHSVValue();
+
             }
             public void onStartTrackingTouch(SeekBar seekBar) {
                 //redValue.setText(Integer.toString(progressChangedValue));
@@ -61,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
                 progressChangedValue = progress;
                 greenValue.setText(Integer.toString(progressChangedValue));
                 getBackColor();
+                getHexValue();
+                getHSVValue();
+
             }
             public void onStartTrackingTouch(SeekBar seekBar) {
                 //redValue.setText(Integer.toString(progressChangedValue));
@@ -76,6 +91,9 @@ public class MainActivity extends AppCompatActivity {
                 progressChangedValue = progress;
                 blueValue.setText(Integer.toString(progressChangedValue));
                 getBackColor();
+                getHexValue();
+                getHSVValue();
+
             }
             public void onStartTrackingTouch(SeekBar seekBar) {
                 //redValue.setText(Integer.toString(progressChangedValue));
@@ -88,11 +106,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getBackColor(){
-
         red_value = redSeeker.getProgress();
         green_value = greenSeeker.getProgress();
         blue_value = blueSeeker.getProgress();
         myLayout.setBackgroundColor( 0xff000000 + red_value * 0x10000 + green_value * 0x100 + blue_value);
+
+    }
+
+    public void getHexValue(){
+        hex = String.format("#%02X%02X%02X", red_value, green_value, blue_value);
+        hexValue.setText(hex);
+        hexValue.setTextColor(Color.parseColor(hex));
+    }
+
+    public void getHSVValue(){
+        hsv = new float[3];
+        red_value = redSeeker.getProgress();
+        green_value = greenSeeker.getProgress();
+        blue_value = blueSeeker.getProgress();
+
+        Color.RGBToHSV(red_value, green_value, blue_value, hsv);
+
+        float hue = hsv[0];
+        float sat = hsv[1] * 100;
+        float val = hsv[2] * 100;
+
+        hueValue.setText(Integer.toString((int) hue) + "\u00b0");
+        satValue.setText(Integer.toString((int) sat) + "%");
+        valValue.setText(Integer.toString((int) val) + "%");
+
+        hueValue.setTextColor(Color.parseColor(hex));
+        satValue.setTextColor(Color.parseColor(hex));
+        valValue.setTextColor(Color.parseColor(hex));
+
     }
 
 
